16a214804b75371131baf1e6938043611fb82a5c
SNA212 <sanggoro.anggoro@gmail.com>
2025-10-27 03:18:40 +0700

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ const CACHE_DURATION_MS = 300 * 1000; // Cache berlaku selama 30 detik
 // ===============================================================
 
 // 3. Middleware
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'Public')));
 app.use(express.json());
 
 // 4. API Endpoint Utama (Dengan Logika Cache)
@@ -290,5 +290,6 @@ function getSourceIcon(domain){const d=domain.toLowerCase(),k={'youtube.com':'<s
     for(const s in k){if(d.includes(s))return k[s]}const i=d.charAt(0).toUpperCase();return`<img class="source-icon" src="https://placehold.co/40x40?text=${i}" alt="${i}" rel="noopener nofollow noreferrer">`}
 
 function randomColor(){const c=['673AB7','3F51B5','2196F3','00BCD4','4CAF50','FF9800'];return c[Math.floor(Math.random()*c.length)]}
+
 
 
